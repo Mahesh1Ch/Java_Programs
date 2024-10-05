07b59a793ed87014b08cb5098c18aa0864bd97cd
Mahesh <maheswarreddychagam555@gmail.com>
2024-10-05 15:52:40 +0530

    Adding the files

---
+++ Java_Practice2/ArrowUp2.java
@@ -0,0 +1,50 @@
+class ArrowUp2
+{
+	public static void main(String[] args)
+	{
+		int row = 5;
+
+		pattern(row);	
+	}
+
+	public static void pattern(int row)
+	{
+		int space=row;
+		int star=0;
+
+		for(int i=1;i<=row*2;i++)
+		{
+			if(space>=1)
+			{
+				space--;
+				star++;
+			}
+
+			for(int j=1;j<=space;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1;j<=star*2-1 && i<=row;j++)
+			{
+				System.out.print("* ");
+			}
+
+			for(int j=1;j<=row*2-1;j++)
+			{
+				if(i>row && i<=row*2 && j>=3 && j<=row*2-3)
+				{
+					System.out.print("* ");
+				}
+				else{
+					System.out.print("  ");
+				}
+			}
+
+			System.out.println();
+
+
+		}
+
+	}
+
+}
